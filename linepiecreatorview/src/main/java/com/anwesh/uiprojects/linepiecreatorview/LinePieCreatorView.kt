4cd4d9b2dc1b1baf22106ec30b2e8e2cae9358e6
Anwesh43 <anweshthecool0@gmail.com>
2020-08-23 14:44:26 +0530

    adding constants and libraries

--- linepiecreatorview/src/main/java/com/anwesh/uiprojects/linepiecreatorview/LinePieCreatorView.kt
+++ linepiecreatorview/src/main/java/com/anwesh/uiprojects/linepiecreatorview/LinePieCreatorView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.linepiecreatorview
 /**
  * Created by anweshmishra on 23/08/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val colors : Array<String> = arrayOf("", "", "", "", "")
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val strokeFactor : Int = 90
+val sizeFactor : Float = 4.6f
+val delay : Long = 20
+val foreColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 45f
