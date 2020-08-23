7a51afe24aaccb3952d63b33722e1ca4219e286f
Anwesh43 <anweshthecool0@gmail.com>
2020-08-23 15:08:23 +0530

    adding LinePieCreator class

--- linepiecreatorview/src/main/java/com/anwesh/uiprojects/linepiecreatorview/LinePieCreatorView.kt
+++ linepiecreatorview/src/main/java/com/anwesh/uiprojects/linepiecreatorview/LinePieCreatorView.kt
@@ -160,4 +160,27 @@ class LinePieCreatorView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LinePieCreator(var i : Int) {
+
+        private var curr : LPCNode = LPCNode(0)
+        private var dir : Int = 1
+
+        fun render(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb  : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
